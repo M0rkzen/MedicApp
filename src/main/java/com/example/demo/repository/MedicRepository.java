1460112b41e298f9904caca0f514217ecd382278
Marius Cota <113298614+M0rkzen@users.noreply.github.com>
2023-09-07 16:12:26 +0300

    Final

--- src/main/java/com/example/demo/repository/MedicRepository.java
+++ src/main/java/com/example/demo/repository/MedicRepository.java
@@ -4,6 +4,10 @@ import com.example.demo.entity.Medic;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MedicRepository extends JpaRepository<Medic, Long> {
+
+    List<Medic> findMedic(String firstName);
 }
--- src/main/java/com/example/demo/repository/PatientRepository.java
+++ src/main/java/com/example/demo/repository/PatientRepository.java
@@ -1,7 +1,14 @@
 package com.example.demo.repository;
 
 import com.example.demo.entity.Patient;
+import com.example.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+
+    List<User> findPatientByFirstName(String firstName);
 }
